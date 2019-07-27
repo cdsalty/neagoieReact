865fb026f6a5ff56bd1d5cea7c740d9226e8c52d
cdsalty <soltiscd@gmail.com>
2019-07-27 14:56:15 -0400

    minor updates

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      string: 'Hello Wiz Christopher'
+    };
+  }
+
+
   render(){
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Wiz Christopher.Soltis
-        </p>
+        <p>{this.state.string}</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
       </header>
     </div>
