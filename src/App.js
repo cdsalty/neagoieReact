e5bd1a8924819b1e7f2761b9772bc2162117ed41
cdsalty <soltiscd@gmail.com>
2019-07-29 15:21:12 -0400

    early process, next is to pass in actual components

--- src/App.js
+++ src/App.js
@@ -1,66 +1,36 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
+
 
 
 class App extends Component {
   constructor(){
-    super();  // will call on Component's built in constructor
-    // the keys/id's will be called with monsters.name
+    super();  // super calls on Component's built in constructor
     this.state = {
-      monsters: [
-        {
-          name: 'Acuna Jr',
-          id: 13
-        },
-        {
-          name: 'Freeman',
-          id: 5
-        },
-        {
-          name: 'Donaldson',
-          id: 20
-        },
-      ]
+      monsters: []
     }
   };
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users').then(response => console.log(response))
+  componentDidMount(){  
+    fetch('https://jsonplaceholder.typicode.com/users') // make the api request
+    .then(response => response.json())  
+    // .then(users => console.log(users)) // to see the array of of users
+    .then(users => this.setState({ monsters: users}));  // will set the value of monsters listed in state above
   }
-
-
-      // <h3 key = { monster.id }>  whatever here <h3>
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h3 key = {monster.id}> {monster.name } </h3>)
-        }
+        <CardList name = "Christopher">
+          {this.state.monsters.map(monster => ( 
+          <h3 key = {monster.id}> {monster.name} </h3>
+          ))}
+        </CardList> 
       </div>
     );
   }
 }
-// function App() {  // simple function that will be called later, inside index.js
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
