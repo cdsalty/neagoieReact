0c58341d48e4c120f014ade52278cc4d02098512
cdsalty <soltiscd@gmail.com>
2019-07-28 23:18:02 -0400

    very basics and basic setup still

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
   constructor(){
     super();
-
+    // the keys/id's will be called with monsters.name
     this.state = {
-      string: 'Hello Wiz Christopher'
-    };
-  }
+      monsters: [
+        {
+          name: 'Acuna Jr',
+          id: 13
+        },
+        {
+          name: 'Freeman',
+          id: 5
+        },
+        {
+          name: 'Donaldson',
+          id: 20
+        },
+      ]
+    }
+  };
 
+      // <h3 key = { monster.id }>  whatever here <h3>
 
-  render(){
+  render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{this.state.string}</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-      </header>
-    </div>
+        {
+          this.state.monsters.map(monster => <h3 key = {monster.id}> {monster.name } </h3>)
+        }
+      </div>
     );
   }
 }
-
 // function App() {  // simple function that will be called later, inside index.js
 //   return (
 //     <div className="App">
