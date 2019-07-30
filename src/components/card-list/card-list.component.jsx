5f683175d1166fa26dc031d733f0e1baebebf89a
cdsalty <soltiscd@gmail.com>
2019-07-30 16:04:31 -0400

    almost complete layout

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import  { Card } from '../card/card.component'
 import './card-list.styles.css';
 
 export const CardList = (props) => ( 
   <div className = 'card-list'>
     {props.monsters.map(monster=> ( 
-      <h3 key={monster.id}>{monster.name}</h3>
+      <Card key={monster.id} monster={monster}/>
     ))}
   </div>
 );
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import './card.styles.css';
+
+export const Card = props => (
+  <div className = 'card-container'>
+    <img 
+    alt = 'monster' 
+    // eslint-disable-next-line 
+    src = {`https://robohash.org/${props.monster.name}?set=set2&size=180x180`}
+    />
+    <h2> {props.monster.name} </h2>
+    <p> {props.monster.email} </p>
+  </div>
+);
