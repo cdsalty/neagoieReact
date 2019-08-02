b60ead30d17664020595964782e56ac392dd3f2a
cdsalty <soltiscd@gmail.com>
2019-08-02 14:33:59 -0400

    search will filter results

--- src/App.js
+++ src/App.js
@@ -21,13 +21,18 @@ class App extends Component {
   }
 
   render() {
+  const { monsters, searchField } = this.state;
+  const filteredMonsters = monsters.filter(monster => 
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
         <input type='search' 
         placeholder='Search Monster' 
-        onChange = {e => this.setState({ searchField: e.target.value }, () => console.log(this.state))} 
-        /> 
-        <CardList monsters={this.state.monsters} />
+        onChange = {(e) => this.setState({ searchField: e.target.value})} /> 
+        {/* <CardList monsters={this.state.monsters} /> */}
+        <CardList monsters = {filteredMonsters} />
       </div>
     );
   }
